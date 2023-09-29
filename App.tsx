208e435b2b41acc72f3757c88f0cc050aaec5948
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-09-29 15:03:15 +0100

    feat: Add SafeAreaView for ios and android

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchContainer}>
+          <Text>Search</Text>
+        </View>
+
+        <View style={styles.listContainer}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  searchContainer: {
+    backgroundColor: "green",
+    padding: 20,
+  },
+  listContainer: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 20,
   },
 });
